d256c4c374a95259b8689ce71d2a2782ce8765b4
AndreasBruhn <1064931@ucn.dk>
2022-05-26 00:22:29 +0200

    creating init footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,89 @@
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+
 const navigation = [
 	{ name: 'About', href: '/about' },
 	{ name: 'Shop', href: '/shop' },
 	{ name: 'Privat & Leveringspolitik', href: '/privacy-policy' },
 ];
 
+const info1 = [
+	{
+		title: 'Bad Seed Brewing',
+		description:
+			'Mikrobryggeri med base lidt uden for Aalborg. Vores mission er at skabe dristige, smagfulde og letdrikkelige øl.',
+		facebook: <FaFacebookF />,
+		instagram: <FaInstagram />,
+	},
+];
+const info2 = [
+	{
+		title: 'Info',
+		smileyName: 'Smiley Rapport',
+		smileyHref: 'https://www.findsmiley.dk/757647',
+		policyName: 'Handels & Leveringspolitik',
+		policyHref: '/privacy-policy',
+	},
+];
+const info3 = [
+	{
+		title: 'Kontakt',
+		companyName: 'Bade Seed	Brewing',
+		address: 'Landkærsvej 4',
+		zip: '9280 Storvorde, Danmark',
+		phone: 'tel:+4530319268',
+		email: 'mailto: info@badseedbrewing.dk',
+	},
+];
+
 export default function Footer() {
 	return (
-		<footer className='bg-white h-auto'>
-			<div className='max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8'>
-				<nav className='flex flex-wrap justify-center'>
-					{navigation.map((item, i) => (
-						<div key={i} className='px-6 py-2'>
-							<a href={item.href} className='text-gray-500 hover:text-gray-900'>
-								{item.name}
+		<footer className='bg-black h-auto text-gray-300'>
+			<div className='grid grid-cols-1 md:grid-cols-3 md:gap-24 max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8'>
+				{info1.map((item, index) => (
+					<div className='flex flex-col' key={index}>
+						<h2 className='text-xl sm:text-3xl text-white'>{item.title}</h2>
+						<p className='my-6'>{item.description}</p>
+						<span className='flex'>
+							{item.facebook}
+							{item.instagram}
+						</span>
+					</div>
+				))}
+				{info2.map((item, index) => (
+					<div className='flex flex-col' key={index}>
+						<h2 className='text-xl sm:text-3xl text-white'>{item.title}</h2>
+						<a href={item.smileyHref} className='text-gray-300 hover:text-gray-900 mt-6'>
+							{item.smileyName}
+						</a>
+						<a href={item.policyHref} className='text-gray-300 hover:text-gray-900'>
+							{item.policyName}
+						</a>
+						<span className='flex'>
+							{item.facebook}
+							{item.instagram}
+						</span>
+					</div>
+				))}
+
+				{info3.map((item, index) => (
+					<div key={index} className='flex flex-col'>
+						<h2 className='text-xl sm:text-3xl text-white'>{item.title}</h2>
+						<p className='mt-4'>{item.address}</p>
+						<p className=''>{item.zip}</p>
+						<span>
+							Telefon:
+							<a href={item.phone} className='text-gray-300 hover:text-gray-900'>
+								+45 30319268
 							</a>
-						</div>
-					))}
-				</nav>
-				<p className='mt-8 text-center text-gray-400'>
-					&copy; 2022 Andreas, Mads, Simone.
-				</p>
+						</span>
+						<span>
+							Email:
+							<a href={item.email} className='text-gray-300 hover:text-gray-900'>
+								info@badseedbrewing.dk
+							</a>
+						</span>
+					</div>
+				))}
 			</div>
 		</footer>
 	);
