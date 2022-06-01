86961287d1a4a42d213e59d8b56637dfd1985fee
AndreasBruhn <1064931@ucn.dk>
2022-06-01 10:18:38 +0200

    fixing the active state of navbar

--- components/Nav_v2.js
+++ components/Nav_v2.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useContext } from 'react';
 import { CartContext } from '../context/shopContext';
+import { useRouter } from 'next/router';
 import MiniCart from './MiniCart';
-import Image from 'next/image';
 import { ShoppingBagIcon } from '@heroicons/react/outline';
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
@@ -14,11 +15,12 @@ const navigation = [
 	{ name: 'Om os', href: '/about', current: false },
 ];
 
-function classNames(...classes) {
+function cn(...classes) {
 	return classes.filter(Boolean).join(' ');
 }
 
 export default function Nav() {
+	const router = useRouter();
 	const { cart, cartOpen, setCartOpen } = useContext(CartContext);
 
 	// functions as a quantity counter
@@ -31,7 +33,7 @@ export default function Nav() {
 		<Disclosure as='nav' className='bg-white'>
 			{({ open }) => (
 				<>
-					<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+					<div className='max-w-7xl mx-auto px-2 sm:px-0'>
 						<div className='relative flex items-center justify-between'>
 							{/* logo  */}
 							<Link href='/' passHref>
@@ -60,22 +62,29 @@ export default function Nav() {
 							</div>
 							<div className='hidden flex-1 md:flex items-center sm:items-stretch sm:justify-end'>
 								<div className='hidden sm:block sm:ml-6'>
-									<div className='flex space-x-4'>
+									<div className='flex items-center mx-auto'>
+										{/* navigation */}
 										{navigation.map((item) => (
-											<a
-												key={item.name}
-												href={item.href}
-												className='px-3 py-2 rounded-md text-sm font-medium text-black hover:text-gray-500'
-												aria-current={item.current ? 'page' : undefined}
-											>
-												{item.name}
-											</a>
+											<li key={item.name} className='px-4 list-none '>
+												<a
+													href={item.href}
+													className={cn(
+														router.pathname == item.href
+															? 'text-gray-700 font-bold border-b-2 border-w border-gray-700'
+															: '',
+														'p-2 hover:border-b-2 hover:outline-none hover:border-gray-700'
+													)}
+													aria-current={item.current ? 'page' : undefined}
+												>
+													{item.name}
+												</a>
+											</li>
 										))}
 										<a
-											className='text-md font-bold cursor-pointer relative'
+											className='text-md font-bold cursor-pointer relative border-l-2 border-gray-700 pl-6'
 											onClick={() => setCartOpen(!cartOpen)}
 										>
-											<ShoppingBagIcon className='h-7 w-7' />{' '}
+											<ShoppingBagIcon className='h-7 w-7 ' />{' '}
 											<span className='bg-black flex items-center justify-center w-6 h-6 text-white rounded-full absolute bottom-[65%] left-[70%]'>
 												{cartQuantity}
 											</span>
@@ -86,6 +95,7 @@ export default function Nav() {
 						</div>
 					</div>
 
+					{/* Mobile menu  */}
 					<Disclosure.Panel className='sm:hidden'>
 						<div className='px-2 pt-2 pb-3 space-y-1'>
 							{navigation.map((item) => (
