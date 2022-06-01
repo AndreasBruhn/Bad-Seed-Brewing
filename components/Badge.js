16e81612b39879cdba86ca7220786dc7c85e16d6
AndreasBruhn <1064931@ucn.dk>
2022-06-01 11:51:48 +0200

    refact css

--- components/Badge.js
+++ components/Badge.js
@@ -5,7 +5,7 @@ export default function Badge() {
 		<span className='hidden absolute lg:flex flex-col justify-center items-center w-36 h-36 bg-black text-white rounded-full rotate-6 z-10 right-[10%] bottom-[68%]'>
 			<h4 className='uppercase font-bold text-4xl'>Mix</h4>
 			<p className='text-[10px] text-center text-gray-300'>Perfekt til ølsmagning</p>
-			<p>
+			<p className='text-center'>
 				6 packs <br /> 12 packs+
 			</p>
 		</span>
