ad433fbafe96f1b9f9507bd1ac19e390a186fa0e
AndreasBruhn <1064931@ucn.dk>
2022-06-02 12:45:20 +0200

    img

--- components/Marquee.js
+++ components/Marquee.js
@@ -4,19 +4,27 @@ import Ticker from 'react-ticker';
 export default function Marquee({ headline }) {
 	return (
 		<div
-			className='flex my-auto w-full md:h-[600px] max-w-2xl mx-auto md:max-w-7xl'
+			className='flex my-auto w-full md:h-[829px] max-w-2xl mx-auto md:max-w-7xl'
 			style={{
 				backgroundImage: 'url(/images/marquee-bg.png)',
 				backgroundRepeat: 'no-repeat',
 				backgroundSize: 'cover',
-				backgroundPosition: ''
+				backgroundPosition: '',
 			}}
 		>
-			<div className='w-full h-full backdrop-blur-xs bg-black/40'>
+			<div className='relative w-full h-full backdrop-blur-xs bg-black/40'>
 				<div className='w-full py-24 md:py-64 '>
 					<Ticker height={120} offset={80}>
-						{() => <h2 className='text-3xl md:text-8xl font-extrabold mr-16 lg:mr-32 text-white'> {headline} </h2>}
+						{() => (
+							<h2 className='text-3xl md:text-8xl font-extrabold mr-16 lg:mr-32 text-white'>
+								{' '}
+								{headline}{' '}
+							</h2>
+						)}
 					</Ticker>
+				</div>
+				<div className='relative  h-64'>
+					<Image src={require('../assets/img/hero_v1.png')} alt={`alt`} layout='fill' objectFit='contain' />
 				</div>
 			</div>
 		</div>
