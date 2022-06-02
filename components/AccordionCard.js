c66380c2f834c9cb4e269118c6c4b33856a5ed79
AndreasBruhn <1064931@ucn.dk>
2022-06-02 11:27:31 +0200

    css refact

--- components/AccordionCard.js
+++ components/AccordionCard.js
@@ -11,26 +11,27 @@ export const AccordionCard = ({ product }) => {
 	const { altText, url } = product.node.images.edges[0].node; // destruct for our product image
 	const linkUrl = `/products`;
 
-	console.log(description);
 	const [isHovered, setIsHovered] = useState(false);
 
 	return (
 		<div
 			onMouseEnter={(e) => setIsHovered(true)}
 			onMouseLeave={(e) => setIsHovered(false)}
-			className='group h-full text-center'
+			className='group h-full text-center '
 		>
 			<div className='w-full relative'>
-				<span className='absolute w-32 h-32 rounded-full bg-red-500 left-[21%] top-[58%]'></span>
+				<span className='absolute w-32 h-32 rounded-full bg-gray-100 left-[21%] top-[58%]'></span>
 				<div className='relative duration-300 group-hover:scale-125 h-64'>
 					<Image src={url} alt={altText} layout='fill' objectFit='contain' />
 				</div>
 			</div>
 			<h3 className='mt-8 text-lg font-medium text-gray-900 uppercase pb-4'>{title}</h3>
-			<p className={` ${isHovered ? 'block line-clamp-4 ' : 'hidden'}`}>{description}</p>
+			<div className="relative duration-200">
+				<p className={`${isHovered ? 'block line-clamp-4' : 'hidden'}`}>{description}</p>
+			</div>
 			<div className='pt-4 flex justify-center items-center acc-btn-container'>
 				<Link href={`${linkUrl}/${handle}`}>
-					<a className='text-center  text-[#001EBD] font-semibold p-2 hover:text-[#001EBD]/80 rounded-sm'>
+					<a className='text-center text-[#001EBD] font-semibold p-2 hover:text-[#001EBD]/80 rounded-sm'>
 						Læs mere
 					</a>
 				</Link>
---
+++ components/FramerSlider.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FramerSlider() {
+  return (
+    <div>FramerSlider</div>
+  )
+}
