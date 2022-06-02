d3524a5a94260349030ee4109eee0e161c7073a0
AndreasBruhn <1064931@ucn.dk>
2022-06-02 13:09:31 +0200

    refact

--- components/Marquee.js
+++ components/Marquee.js
@@ -14,7 +14,7 @@ export default function Marquee({ headline }) {
 		>
 			<div className='relative w-full h-full '>
 				<div className='w-full whitespace-nowrap py-24 md:py-64 '>
-					<Ticker height={340} offset={80} speed={7}>
+					<Ticker height={340} offset={80} speed={8}>
 						{() => (
 							<h2 className='text-3xl md:text-7xl lg:text-[300px] font-extrabold mr-16 lg:mr-64 text-white'>
 								{' '}
