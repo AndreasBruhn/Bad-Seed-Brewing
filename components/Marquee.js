3340d04fddc113b8b35ab2b6035665b477a53b7a
AndreasBruhn <1064931@ucn.dk>
2022-06-03 17:18:33 +0200

    fixing overflow issue on mobile

--- components/Marquee.js
+++ components/Marquee.js
@@ -4,26 +4,26 @@ import Ticker from 'react-ticker';
 export default function Marquee({ headline }) {
 	return (
 		<div
-			className='flex my-auto w-full md:h-[829px] max-w-2xl mx-auto md:max-w-7xl'
+			className='flex my-auto w-full h-[400px] md:h-[829px] max-w-2xl mx-auto md:max-w-7xl'
 			style={{
 				backgroundImage: 'url(/images/marquee-bg.png)',
 				backgroundRepeat: 'no-repeat',
 				backgroundSize: 'cover',
-				backgroundPosition: '',
+				backgroundPosition: 'center',
 			}}
 		>
 			<div className='relative w-full h-full '>
 				<div className='w-full whitespace-nowrap py-24 md:py-64 '>
 					<Ticker height={340} offset={80} speed={10}>
 						{() => (
-							<h2 className='text-3xl uppercase md:text-7xl lg:text-[300px] font-extrabold mr-16 lg:mr-64 text-white'>
+							<h2 className='text-3xl uppercase md:text-7xl lg:text-[150px] font-extrabold mr-16 lg:mr-64 text-white'>
 								{' '}
 								{headline}{' '}
 							</h2>
 						)}
 					</Ticker>
 				</div>
-				<div className='relative h-28 w-28 left-[87%] bottom-[20%]'>
+				<div className='hidden lg:block relative h-28 w-28 left-[87%] bottom-[20%]'>
 					<Image src={require('../assets/img/white-logo.png')} alt={`alt`} layout='fill' objectFit='contain' />
 				</div>
 			</div>
